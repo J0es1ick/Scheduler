3cf586efc0d734c66af8f8b65267496a2b8d1aa1
J0es1ick <mrjoeslick@gmail.com>
2026-08-14 06:34:15 +0300

    add migrator and production compose safeguards

---
+++ docker/telegram-mock/telegram_api.py
@@ -0,0 +1,30 @@
+import json
+import time
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+
+
+class TelegramAPIHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        length = int(self.headers.get("Content-Length", "0"))
+        if length:
+            self.rfile.read(length)
+
+        method = self.path.rsplit("/", 1)[-1]
+        if method == "getUpdates":
+            time.sleep(0.25)
+            result = []
+        else:
+            result = True
+
+        body = json.dumps({"ok": True, "result": result}).encode()
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Content-Length", str(len(body)))
+        self.end_headers()
+        self.wfile.write(body)
+
+    def log_message(self, _format, *_args):
+        return
+
+
+ThreadingHTTPServer(("0.0.0.0", 8080), TelegramAPIHandler).serve_forever()
